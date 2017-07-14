1c912acce9711462107840f1377705610f347db5
slam24 <luismartinez242011@gmail.com>
2017-07-14 04:38:18 -0400

    feat: Config

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/name',function(){
+	$name = config('app.name');
+	return $name;
+});
+
+Route::get('/time',function(){
+	$time = config('app.timezone');
+	return $time;
+});
+
+Route::get('/runtime',function(){
+	config(['app.timezone' => 'America/Mexico_City']);
+	$time = config('app.timezone');
+	return $time;
+});
